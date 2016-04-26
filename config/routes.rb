62e87d6d7653c115289a4876a8f9dd24c1d6d7f3
90078ITU <gursoyercan81@students.itu.edu>
2016-04-25 23:58:36 -0700

    Finished Dog model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :dogs
+
+  get 'dogs/new'
+
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
